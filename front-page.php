5acbd533d3b00ae739deada225ac6c8736f58515
Andres Vega <gydoar@hotmail.com>
2015-04-16 12:06:18 -0500

    primer commit

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+	<div class="body__1">
+		<div class="line__oro"></div>
+		<div class="marco container">
+			<h3><strong>WARNING</strong></h3>
+			<div class="width--500">
+				
+				<p>This web site escorts agency contains artistic depictions of nudity.</p>
+
+				<p>To continue, you must read and agree to the terms and conditions below. By entering this site, you represent and promise each of the following: that you are an adult of legal age (and at least 18 years old) in your state or country; that you are not offended by photographs depicting nudity and sexual situations; that it legal to receive and view adult images in the locale and country in which you reside; that you will never make any of the materials in this web site available to minors; that you will not publish, use or  distribute any of the contents of this site in any media.</p>
+
+				<p>That you will not publish, use or distribute any of the contents of this site in any media.</p>
+			
+			</div>
+
+		</div>
+	</div>
+
+<?php get_footer(); ?>
