3bd90e825870b7aaa516703270030ae673d984a6
Andres Vega <gydoar@hotmail.com>
2015-04-16 17:36:31 -0500

    Iniciando galeria de imagenes en el home

--- page-home.php
+++ page-home.php
@@ -12,6 +12,31 @@
 			<div><img src="<?php bloginfo('template_url' ); ?>/library/img/orn-1.png" alt=""></div>
 			<p>DUCHESS LUXURY SCORTS Is a agency based Top-quality high class Service for private capricious <br>of gentlemen who demand the very best things in life. </p>
 			<p>Our women First Class come from all Europe, London, American, Russian, Asia. Escort Agency <br>around the world offer the best girls , they are open-minded, beautiful, fun loving, charming, with <br>brains, well educated, perfect manners and much more what every men have deserve.</p>
+
+			<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/btn-read.png" alt=""></a>
+		</div>
+		<div class="cuadro--top--model">
+			<div class="ten columns offset-by-one">
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div>
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+				</div>
+
+			</div>
+
+
+			
 		</div>
 	</div>
 
