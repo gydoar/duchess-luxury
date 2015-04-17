48ac9fb1f312b0aecf9a119eace9ccd1439d7bcc
Andres Vega <gydoar@hotmail.com>
2015-04-17 16:48:28 -0500

    pagina de taxonomies o categoria

---
+++ taxonomy-scortstaxonomies.php
@@ -0,0 +1,106 @@
+<?php get_header('page'); ?>
+
+
+<div class="color__black">
+	<div class="line__oro_2"></div>
+
+	<div class="container">
+		
+		<div class="cuadro--top--model">
+			<div class="ten columns offset-by-one portfolio margin__buttom">
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+			</div>
+
+			<!-- segunda fila -->
+			<div class="ten columns offset-by-one margin__buttom">
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+			</div>
+
+			<!-- tercera fila -->
+			<div class="ten columns offset-by-one">
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+				<div class="three columns">
+					<a href="#"><img src="<?php bloginfo('template_url' ); ?>/library/img/img-scorts.png" alt=""></a>
+					<div class="btn-portfolio">
+						<a href="#">Aleja</a>
+					</div>
+				</div>
+			</div>
+
+
+			
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="btn-see-more">
+			<a href="#">SEE MORE</a>
+		</div>
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
