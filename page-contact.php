1583a7f7d9cff753756bf6b8cef04ef601f15a07
Andres Vega <gydoar@hotmail.com>
2015-04-21 11:08:08 -0500

    pagina de contacto

--- page-contact.php
+++ page-contact.php
@@ -1,4 +1,19 @@
 <?php get_header('page'); ?>
 
+	<div class="color__black">
+		<div class="line__oro_2"></div>
+		<div class="container">
+			<div class="marco__contact">
+				<div class="ten columns offset-by-one">
+					<div class="six columns">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ad esse a porro temporibus accusamus magnam, excepturi suscipit atque laudantium, minus aut animi minima reprehenderit aliquid eligendi eius fuga officia?
+					</div>
+					<div class="six columns">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ad esse a porro temporibus accusamus magnam, excepturi suscipit atque laudantium, minus aut animi minima reprehenderit aliquid eligendi eius fuga officia?
+					</div>
+				</div>		
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
